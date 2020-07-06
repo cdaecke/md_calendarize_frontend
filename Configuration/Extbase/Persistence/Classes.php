52ed87682da18ec10535a70ce64a35b0cfc61048
chris <typo3@mediadreams.org>
2020-07-06 18:03:45 +0200

    [TASK] add Configuration/Extbase/Persistence/Classes.php for TYPO3 10 compatibility

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    \Mediadreams\MdCalendarizeFrontend\Domain\Model\Category::class => [
+        'tableName' => 'sys_category',
+    ],
+
+    \Mediadreams\MdCalendarizeFrontend\Domain\Model\Event::class => [
+        'tableName' => 'tx_calendarize_domain_model_event',
+    ],
+];
