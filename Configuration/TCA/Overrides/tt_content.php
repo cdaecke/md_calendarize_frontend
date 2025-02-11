017818f9cc4d8f78777f675dc30a0bdcac8fa790
chris <typo3@mediadreams.org>
2025-02-11 10:44:24 +0100

    [TASK] update plugin configuration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,17 +2,21 @@
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
+
 defined('TYPO3') || die('Access denied.');
 
 $pluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'MdCalendarizeFrontend',
     'Frontend',
-    'Calendarize frontend'
+    'LLL:EXT:md_calendarize_frontend/Resources/Private/Language/locallang.xlf:mdcalendarizefrontend_frontend.name',
+    'md_calendarize_frontend-plugin-frontend',
+    null,
+    'LLL:EXT:md_calendarize_frontend/Resources/Private/Language/locallang.xlf:mdcalendarizefrontend_frontend.description',
 );
 
 ExtensionManagementUtility::addToAllTCAtypes(
     'tt_content',
-    '--div--;Configuration,pi_flexform,',
+    '--div--;LLL:EXT:md_calendarize_frontend/Resources/Private/Language/locallang.xlf:tt_content.tab.configuration,pages,',
     $pluginSignature,
     'after:subheader',
 );
